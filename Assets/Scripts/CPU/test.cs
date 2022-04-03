1cd0dec6f1b118d60c5d554d8328113ca1cbf548
liwuchen <liwch@shanghaitech.edu.cn>
2022-04-03 22:39:29 +0800

    add MathNet and write log to file

--- Assets/Scripts/CPU/test.cs
+++ Assets/Scripts/CPU/test.cs
@@ -1,13 +1,16 @@
 using UnityEngine;
 using System.Collections;
+using System.IO;
 using Matrix = MathNet.Numerics.LinearAlgebra.Matrix<double>;
 using Vector = MathNet.Numerics.LinearAlgebra.Vector<double>;
+
 
 
 public class test : MonoBehaviour
 {
     void Start()
     {
+        // matrix computation
         Matrix A = Matrix.Build.DenseOfArray(new double[,] {
              {1,0,0},
              {0,2,0},
@@ -17,5 +20,22 @@ public class test : MonoBehaviour
 
         Vector x = A.Solve(b);
         Debug.Log("x = A^-1b: " + x);
+
+        // write to log.txt and read it again
+        using (StreamWriter sw = new StreamWriter("log.txt"))
+        {
+            sw.Write("Start of file.\n");
+            sw.Write(x);
+            sw.Write("End of file.");
+        }
+
+        string line = "";
+        using (StreamReader sr = new StreamReader("log.txt"))
+        {
+            while ((line = sr.ReadLine()) != null)
+            {
+                print(line);
+            }
+        }
     }
 }
